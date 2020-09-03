69005ab86a0101c49e32e452e42d4b6fc9923ec5
thienphuc <phuclt1976226@gmail.com>
2020-09-03 15:32:43 +0700

    Xong phần đổ danh sách khách hàng

--- app/src/main/java/com/example/hair/activity/MainActivity.java
+++ app/src/main/java/com/example/hair/activity/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.hair.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.hair.R;
+import com.example.hair.adapter.AllKhachHangAdapter;
+import com.example.hair.model.KhachHang;
+import com.example.hair.server.APIService;
+import com.example.hair.server.DataService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,14 +31,36 @@ public class MainActivity extends AppCompatActivity {
     TextView tvNam, tvNu;
     EditText edtTenKH, edt3SDT;
     RecyclerView rvDanhsachKH;
+    AllKhachHangAdapter allKhachHangAdapter;
+    ArrayList<KhachHang> mangkhachhang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Anhxa();
+        GetData();
 
+    }
 
+    private void GetData() {
+        DataService dataService = APIService.getService();
+        Call<List<KhachHang>> callback = dataService.GetAllKH();
+        callback.enqueue(new Callback<List<KhachHang>>() {
+            @Override
+            public void onResponse(Call<List<KhachHang>> call, Response<List<KhachHang>> response) {
+                mangkhachhang = (ArrayList<KhachHang>) response.body();
+//                Log.d("AAA", mangkhachhang.get(0).getTenKH());
+                allKhachHangAdapter = new AllKhachHangAdapter(MainActivity.this, mangkhachhang);
+                rvDanhsachKH.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                rvDanhsachKH.setAdapter(allKhachHangAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<KhachHang>> call, Throwable t) {
+
+            }
+        });
     }
 
     private void Anhxa() {
---
+++ app/src/main/java/com/example/hair/adapter/AllKhachHangAdapter.java
@@ -0,0 +1,60 @@
+package com.example.hair.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.hair.R;
+import com.example.hair.model.KhachHang;
+
+import java.util.ArrayList;
+
+public class AllKhachHangAdapter extends RecyclerView.Adapter<AllKhachHangAdapter.ViewHolder> {
+
+    Context context;
+    ArrayList<KhachHang> khachHangArrayList;
+
+    public AllKhachHangAdapter(Context context, ArrayList<KhachHang> khachHangArrayList) {
+        this.context = context;
+        this.khachHangArrayList = khachHangArrayList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater= LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.dong_khachhang,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        KhachHang khachHang = khachHangArrayList.get(position);
+        holder.tvTenKH.setText(khachHang.getTenKH());
+        holder.tvGioitinhKH.setText(khachHang.getGioiTinhKH());
+        holder.tvSDTKH.setText(khachHang.getSoDienThoaiKH());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return khachHangArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tvTenKH, tvGioitinhKH, tvSDTKH, tvSolancattoc;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTenKH = itemView.findViewById(R.id.tvdongtenkhachhang);
+            tvGioitinhKH = itemView.findViewById(R.id.tvdonggioitinhkhachhang);
+            tvSDTKH = itemView.findViewById(R.id.tvdongsdtkhachhang);
+            tvSolancattoc = itemView.findViewById(R.id.tvdongsolancattoc);
+        }
+    }
+}
